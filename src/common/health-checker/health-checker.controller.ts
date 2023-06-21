0f4b7116adbf88d3a8030495efa0b135c2dac748
chanyang <chanyang721@gmail.com>
2023-06-21 17:18:00 +0900

    [ feat ] app health-checker

---
+++ src/common/health-checker/health-checker.controller.ts
@@ -0,0 +1,28 @@
+import { Controller, Get, HttpException, Post }                                       from "@nestjs/common";
+import { HealthCheck, HealthCheckResult, HealthCheckService, TypeOrmHealthIndicator } from "@nestjs/terminus"
+
+
+
+@Controller('health-checker')
+export class HealthCheckerController {
+    constructor( private healthCheckerService: HealthCheckService, private typeOrmIndicator: TypeOrmHealthIndicator ) {
+    }
+
+    /**
+     *  {
+     *     "status": 'error' | 'ok' | 'shutting_down',
+     * }
+     * */
+    @Get()
+    @HealthCheck()
+    async healthCheck(): Promise<HealthCheckResult> {
+        return this.healthCheckerService.check([
+            () => this.typeOrmIndicator.pingCheck("database", { timeout: 1500 }),
+        ]);
+    }
+
+    @Post()
+    async healthCheckPost(): Promise<any> {
+        throw new HttpException("Not Found", 404);
+    }
+}
---
+++ src/common/health-checker/health-checker.module.ts
@@ -0,0 +1,13 @@
+import { Module }                  from '@nestjs/common';
+import { HealthCheckerController } from './health-checker.controller';
+import { TerminusModule }          from '@nestjs/terminus'
+
+
+
+@Module({
+    imports    : [ TerminusModule ],
+    controllers: [ HealthCheckerController ],
+    providers  : [],
+})
+export class HealthCheckerModule {
+}
